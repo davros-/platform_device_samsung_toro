0126bed129a76a4dfb13a899530074534b82b0df
cphelps76 <cphelps76@gmail.com>
2013-03-01 00:09:19 -0600

    turbo toro

--- device.mk
+++ device.mk
@@ -31,7 +31,44 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.cdma.home.operator.alpha=Verizon \
 	ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
 	ro.cdma.data_retry_config=default_randomization=2000,0,0,120000,180000,540000,960000 \
-	ro.cdma.otaspnumschema=SELC,1,80,99
+	ro.cdma.otaspnumschema=SELC,1,80,99 \
+        windowsmgr.max_events_per_sec=350 \
+        ro.kernel.android.checkjni=0 \
+        ro.media.enc.jpeg.quality=100 \
+        ro.config.nocheckin=1 \
+        debug.performance.tuning=1 \
+        persist.service.swiqi.enable=1 \
+        video.accelerate.hw=1 \
+        ro.ril.enable.3g.prefix=1 \
+        ro.ril.hep=1 \
+        ro.ril.hsxpa=3 \
+        ro.ril.enable.dtm=1 \
+        ro.ril.gprsclass=12 \
+        ro.ril.hsdpa.category=8 \
+        ro.ril.enable.a53=1 \
+        ro.ril.hsupa.category=5 \
+        persist.sys.purgeable_assets=1 \
+        pm.sleep_mode=1 \
+        ro.home_app_adj=1 \
+        ro.ext4fs=1 \
+        persist.sys.use_dithering=0 \
+        ro.ril.disable.power.collapse=0 \
+        dalvik.vm.heapstartsize=128m \
+        dalvik.vm.heapgrowthlimit=320m \
+        dalvik.vm.heapsize=512m \
+        dalvik.vm.execution-mode=int:jit \
+        dalvik.vm.heaputilization=0.05 \
+        dalvik.vm.heapidealfree=8388608 \
+        dalvik.vm.heapconcurrentstart=2097152 \
+        dalvik.vm.verify-bytecode=false \
+        ro.dalvik.vm.checkjni=0 \
+        dalvik.vm.dexopt-flags=v=n,o=v \
+        debug.composition.type=gpu \
+        ro.max.fling_velocity=15000 \
+        ro.min.fling_velocity=8000 \
+        ro.config.hw_fast_dormancy=1 \
+        net.dns1=8.8.4.4 \
+        net.dns2=8.8.8.8 
 
 $(call inherit-product, device/samsung/tuna/device.mk)
 $(call inherit-product-if-exists, vendor/samsung/toro/device-vendor.mk)
